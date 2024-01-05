d7a98ab7b8782534d25edc79f340615018e518d4
Daniela Karmova <daniela.krmv@abv.bg>
2024-01-05 14:29:04 +0200

    added portfolio projects files

---
+++ jobs/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Job(models.Model):
+
+    # Images
+    image = models.ImageField(upload_to='images/')
+
+    # Summary
+    summary = models.CharField(max_length=10000)
+
+    def __str__(self):
+        return self.summary
